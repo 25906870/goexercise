88e7a0582713851659e9064059f43457692f93f0
g <g25906870@gmail.com>
2019-09-20 11:35:37 +0800

    strstrpro

--- src/strRelated/strStr.go
+++ src/strRelated/strStr.go
@@ -1,40 +1,49 @@
 package strRelated
 
-import (
-	
-)
-
 func MystrStr(haystack string, needle string) int {
-    if len(needle) == 0{
-        return 0
-    }
-    if len(haystack) == 0{
-        return -1
-    }
-    
-    hlen := len(haystack)
-    nlen := len(needle)
-    
-    if hlen <nlen{
-        return -1
-    }
-    
-    for i:=0;i<hlen;i++{
-        if haystack[i] == needle[0]{
-            j := 1
-            tmp :=  i + 1
-            for ;j<nlen && i<hlen;{
-                if haystack[tmp] != needle[j] {
-                    break;
-                }else{
-                    tmp++
-                    j++
-                }
-            }
-            if j == nlen{
-                return i
-            }
-        }
-    } 
-    return -1
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) == 0 {
+		return -1
+	}
+
+	hlen := len(haystack)
+	nlen := len(needle)
+
+	if hlen < nlen {
+		return -1
+	}
+
+	for i := 0; i < hlen-nlen+1; i++ {
+		if haystack[i] == needle[0] {
+			j := 1
+			tmp := i + 1
+			for j < nlen && tmp < hlen {
+				if haystack[tmp] != needle[j] {
+					break
+				} else {
+					tmp++
+					j++
+				}
+			}
+			if j == nlen {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func MystrStrPro(haystack string, needle string) int {
+
+	nlen := len(needle)
+
+	for i := 0; i < len(haystack)-nlen+1; i++ {
+		if needle == haystack[i:i+nlen] {
+			return i
+		}
+	}
+
+	return -1
 }
